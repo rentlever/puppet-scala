238a0125c72fd21601c1ccc3a4e16079e7d281df
jeguizabal <jose@rentlever.com>
2014-12-09 12:02:07 -0800

    adding to modify version

---
+++ README.md
@@ -0,0 +1,20 @@
+# Puppet Module for Scala
+
+Installs scala.
+
+## Usage
+
+```puppet
+boxen::scala { 'scala':
+  salutation => 'fam'
+}
+```
+
+## Required Puppet Modules
+
+* `boxen`
+
+## Development
+
+Write code. Run `script/cibuild` to test it. Check the `script`
+directory for other useful tools.
---
+++ manifests/init.pp
@@ -0,0 +1,48 @@
+class scala {
+
+  require wget
+
+  $version = '2.10.3'
+
+  file { '/usr/local':
+    ensure => directory,
+  }
+
+  file { "/tmp/scala-${version}.tgz":
+    ensure => present,
+    require => Exec['Fetch scala'],
+  }
+
+  exec { 'Fetch scala':
+    cwd => '/tmp',
+    command => "wget http://www.scala-lang.org/files/archive/scala-${version}.tgz",
+    creates => "/tmp/scala-${version}.tgz",
+    path    => ['/opt/boxen/homebrew/bin'],
+    require => File['/usr/local'];
+  }
+
+  exec { 'Extract scala':
+    cwd     => '/usr/local',
+    command => "tar xvf /tmp/scala-${version}.tgz",
+    creates => "/usr/local/scala-${version}",
+    path    => ['/usr/bin'],
+    require => Exec['Fetch scala'];
+  }
+
+  file { "/usr/local/scala-${version}":
+    require => Exec['Extract scala'];
+  }
+
+  file { '/usr/local/scala':
+    ensure  => link,
+    target  => "/usr/local/scala-${version}",
+    require => File["/usr/local/scala-${version}"];
+  }
+  
+  file { '/opt/boxen/bin/scala': 
+    ensure => link,
+    target  => '/usr/local/scala/bin/scala',
+    require => File['/usr/local/scala'];
+  }
+
+}
